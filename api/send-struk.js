56cb249b81c05829269a34c1b91b99bbdb882b96
kopiiji12-a11y <kopiiji12@gmail.com>
2026-06-16 19:33:33 +0700

    Update send-struk.js

--- api/send-struk.js
+++ api/send-struk.js
@@ -1,18 +1,20 @@
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        return res.status(405).json({
-            success: false,
-            message: 'Method not allowed'
+    if (req.method === 'GET') {
+        return res.status(200).json({
+            success: true,
+            message: 'API aktif'
         });
     }
 
-    const data = req.body;
+    if (req.method === 'POST') {
+        return res.status(200).json({
+            success: true,
+            data: req.body
+        });
+    }
 
-    console.log('Struk masuk:', data);
-
-    return res.status(200).json({
-        success: true,
-        message: 'Data diterima',
-        data
+    return res.status(405).json({
+        success: false,
+        message: 'Method not allowed'
     });
 }
